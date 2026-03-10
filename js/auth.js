428a49ea237e4443b3a26c1bf1d710073c6e2c50
officeofcapankaj <officeofcapankaj@gmail.com>
2026-03-10 18:12:49 +0530

    Create auth.js

---
+++ js/auth.js
@@ -0,0 +1,35 @@
+async function signup() {
+
+  const email = document.getElementById("signupEmail").value;
+  const password = document.getElementById("signupPassword").value;
+
+  const { data, error } = await supabase.auth.signUp({
+    email,
+    password
+  });
+
+  if (error) {
+    alert(error.message);
+    return;
+  }
+
+  alert("Signup successful. Please check email.");
+}
+
+async function login() {
+
+  const email = document.getElementById("loginEmail").value;
+  const password = document.getElementById("loginPassword").value;
+
+  const { data, error } = await supabase.auth.signInWithPassword({
+    email,
+    password
+  });
+
+  if (error) {
+    alert(error.message);
+    return;
+  }
+
+  window.location.href = "/dashboard.html";
+}
